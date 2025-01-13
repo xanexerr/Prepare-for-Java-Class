fe6618dbf4472173254e33fc4d3c85f8954a138c
Natthapumin Klammat <natthapumin.klammat@gmail.com>
2025-01-13 13:44:51 +0700

    week2/1

---
+++ test.java
@@ -0,0 +1,13 @@
+public class test {
+     public static void main(String[] args) {
+
+          System.err.println(5 + 2 * 4);
+          System.err.println(10 / 2 - 3);
+          System.err.println(8+12*2-4);
+          System.err.println(10/(5-3));
+          System.err.println((4+17)%2-1);
+          System.err.println((6-3)*(2+7)/3);
+          System.err.println(12+6/3%2);
+     }
+
+}
